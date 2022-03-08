6488ede76d789db482b0e638f64cf0db1bc1de88
duyamn <99411473+duyamn@users.noreply.github.com>
2022-03-08 16:23:37 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/passwordprotecteddiaryapp/MenuOfNotes.java
@@ -0,0 +1,14 @@
+package com.example.passwordprotecteddiaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuOfNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_of_notes);
+    }
+}
