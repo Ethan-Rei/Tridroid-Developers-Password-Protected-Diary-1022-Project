229eb669282fe99bd037b8bb7fe8504fd1f4695d
aijay <Aijaysarma14@gmail.com>
2022-03-11 03:45:11 -0500

    Password Protected Diary App

--- app/src/main/java/com/example/passwordprotecteddiaryapp/MainActivity.java
+++ app/src/main/java/com/example/passwordprotecteddiaryapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.passwordprotecteddiaryapp;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,4 +30,24 @@ public class MainActivity extends AppCompatActivity
         Intent intent = new Intent(MainActivity.this, MenuOfNotes.class);
         startActivity(intent);
     }
+
+    public void createEnterPasswordDialog()
+    {
+        dialogBuilder = new AlertDialog.Builder(this);
+        View popup = getLayoutInflater().inflate(R.layout.enter_password_prompt, null);
+
+        EditText password = (EditText) popup.findViewById(R.id.editTextTextPassword);
+        Button confirm = (Button) popup.findViewById(R.id.confirm);
+
+        dialogBuilder.setView(popup);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        confirm.setOnClickListener(view -> openNotesMenu());
+
+
+
+    }
+
+
 }
